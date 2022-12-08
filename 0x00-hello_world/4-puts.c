0f09c5d4f03a0aff4145c12453a490d6c280dc16
brainzcode <98150590+brainzcode@users.noreply.github.com>
2022-12-08 11:06:30 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry Point
+ * main -Entry Point
  *
- * Return: Always 0 (Success)
- *
+ * Return: Always 0 (Sucess)
  */
-
-int main(void) {
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
