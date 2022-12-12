918a0c7b7d3c0cf591645ce6bbcf010c8d7afbe9
brainzcode <98150590+brainzcode@users.noreply.github.com>
2022-12-11 23:48:49 -0800

    a program that prints all possible different combinations of two digits.

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,24 +1,32 @@
 #include <stdio.h>
-
 /**
- * main - program that prints numbers from 0 to 99.
- * Return: 0
+ * main - program that prints all possible different combinations of 2 digits
+ * Return: Always 0 (Success).
  */
 int main(void)
 {
 	int num = 0;
+	int b;
+	int d;
 
 	while (num <= 99)
 	{
-		putchar(num / 10 + '0');
-		putchar(num % 10 + '0');
-		if (num != 99)
+		b = (num / 10 + '0');
+		d = (num % 10 + '0');
+
+		if (b < d)
 		{
-			putchar(',');
-			putchar(' ');
+			putchar(b);
+			putchar(d);
+
+			if (num != 89)
+			{
+				putchar(',');
+				putchar(' ');
 			}
-			++num;
 		}
+		++num;
+	}
 putchar('\n');
 return (0);
 }
