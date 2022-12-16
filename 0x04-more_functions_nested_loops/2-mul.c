aaf7fcf9a3e453fa04a75cb494893d5aa2de7a3e
brainzcode <98150590+brainzcode@users.noreply.github.com>
2022-12-15 20:43:32 -0800

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* mul - multiplies two numbers
+* @a: first number
+* @b: second number
+* Return: returns result
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+
+
+#include <stdio.h>
+#include "main.h"
+/**
+*print_numbers - function that print all numbers from
+*0 to 9
+*
+*Return: returns nothing
+*/
+void print_numbers(void)
+{
+	int n;
+	for (n = 48; n < 58; n++)
+	{
+		_putchar(n);
+	}
+	_putchar(10);
+}
