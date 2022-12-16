7301914c06e021571c1b94c75520e6a29de338f2
brainzcode <98150590+brainzcode@users.noreply.github.com>
2022-12-15 20:04:49 -0800

    a function that checks for a digit (0 through 9)

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,5 +7,16 @@
  */
 
 int _isupper(int);
+int _isdigit(int);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+int _putchar(char);
+int mul(int, int);
 
 #endif /*MAIN_H*/
