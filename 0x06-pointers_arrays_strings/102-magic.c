7c1c279494a5c2c836c35a6af0ad478ff5996ade
brainzcode <98150590+brainzcode@users.noreply.github.com>
2023-01-13 04:02:37 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+
 /**
- * main - prints a[2] = 98.
- * Return: Always 0.
- */
+ * main - Prints a magic file 😂 a[2] = 98.
+ * Return: Returns 0.
+*/
 int main(void)
 {
 	int n;
@@ -11,16 +12,16 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-	/*
-	 * write your line of code here...
-	 * Remember:
-	 * - you are not allowed to use a
-	 * - you are not allowed to modify p
-	 * - only one statement
-	 * - you are not allowed to code anything else than this line of code
-	 */
+    /*
+     * write your line of code here...
+     * Remember:
+     * - you are not allowed to use a
+     * - you are not allowed to modify p
+     * - only one statement
+     * - you are not allowed to code anything else than this line of code
+     */
 	*(p + 5) = 98;
-	/* ...so that this prints 98\n */
+    /* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
